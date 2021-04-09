0d35dff5f58beeb9af6dbdb607560c24ca8a50d5
xykzzzz <1105054453@qq.com>
2021-04-09 20:51:56 +0800

    剑指offer

---
+++ offer3/offer11.java
@@ -0,0 +1,12 @@
+package aa;
+
+public class offer11 {
+    public int NumberOf1(int n) {
+        int count=0;
+        while (n!=0){
+           count++;
+            n=(n&(n-1));
+        }
+        return count;
+    }
+}
---
+++ offer3/offer12.java
@@ -0,0 +1,27 @@
+package aa;
+
+import 链表.ListNode;
+
+public class offer12 {
+    public ListNode FindKthToTail (ListNode pHead, int k) {
+        // write code here
+        if(k<=0||pHead==null){
+            return null;
+        }
+        ListNode fast=pHead;
+        ListNode slow=pHead;
+        while (k-1>0){
+            if(fast.next!=null){
+                fast=fast.next;
+                k--;
+            }else {
+                return null;
+            }
+        }
+        while (fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+}
